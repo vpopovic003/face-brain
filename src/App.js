2d3c23e000f1a648b1bd05e919a76b5bdc9d7dd1
Vladimir Popovic <vpopovic003@gmail.com>
2023-07-14 22:12:26 +0200

    place homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import ParticlesBg from 'particles-bg'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div>
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+        </div>
+        <ParticlesBg type="circle" bg={true} />
+      </>
+
     </div>
   );
 }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className='tilt'>
+        <div style={{ height: '150px', backgroundColor: 'white' }}>
+          <img style={{paddingTop: '25px'}} src={brain} alt='logo'/>
+        </div>
+      </Tilt>
+    </div>
+  )
+}
+
+export default Logo
