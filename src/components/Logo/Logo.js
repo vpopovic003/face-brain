df9c759bf9522124e36688b8dfc21ba5f641d688
Vladimir Popovic <vpopovic003@gmail.com>
2023-07-17 19:23:12 +0200

    center logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className='tilt'>
+      <Tilt className='tilt center'>
         <div style={{ height: '150px', backgroundColor: 'white' }}>
           <img style={{paddingTop: '25px'}} src={brain} alt='logo'/>
         </div>
